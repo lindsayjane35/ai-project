fd568646af6ea17df172299cfb71f81e42ae194c
Lindsay <lindsayjane35@gmail.com>
2024-03-20 20:13:20 -0700

    added API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateStory(event) {
-  event.preventDefault();
-
+function displayStory(response) {
   new Typewriter("#story", {
-    strings: "Story here...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateStory(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a83afd1b400db4a3f9fc3t5a4a5c272o";
+  let prompt = `Generate a story about ${instructionsInput.value}`;
+  let context =
+    "User Instructions: You are an expert on scary stories. Your mission is to generate a scary story in basic HTML in as few words as possible.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayStory);
 }
 
 let storyElement = document.querySelector("#generator-form");
